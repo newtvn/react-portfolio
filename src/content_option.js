749f93f1ff2fc3066f9b69b167e28248e0064878
newtvn <newtonbryan12428@gmail.com>
2024-02-25 22:16:45 +0300

    portfolio update 1

---
+++ src/content_option.js
@@ -0,0 +1,169 @@
+const logotext = "Newton Brian";
+const meta = {
+    title: "Newton Brian Nyongesa",
+    description: "Newton Brian Nyongesa _ Full stack devloper,currently working interning at Sky World Limited",
+};
+
+const introdata = {
+    title: "Meet Newton Brian Nyongesa",
+    animated: {
+        first: "Coder, Innovator, tech enthusiast",
+        second: "Web-fanatic",
+        third: "Full stack developer",
+    },
+    description: "Computer science majour, French minor with a key interest in all systems tech and inovation",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "meet the man ",
+    aboutme:  "An energetic Web Development Intern with a keen interest in web and mobile development, I possess a strong foundation in front-end and back-end technologies, including HTML, CSS, JavaScript, and responsive design. My passion for technology drives me to seek out the latest in emerging trends, with a particular focus on the integration of AI and machine learning to enhance user interactions across platforms. I am collaborative, enjoy participating in team-driven environments, and actively contribute to open-source projects and hackathons. Eager to join a forward-thinking company, I aim to contribute innovative ideas and coding expertise to a team that values the intersection of technology and innovative digital solutions.",
+};
+const worktimeline = [
+    {
+      jobtitle: "Full stack Development Intern",
+      where: "Skyworld Limited",
+      date: "Present",
+      description: "Engaged in full-stack web development projects, utilizing modern frameworks and languages to create responsive and user-centric solutions."
+    },
+    {
+      jobtitle: "Oracle Hackathon Runner-Up",
+      where: "Oracle Onboarded Hackathon",
+      date: "2022",
+      description: "Collaborated with a team to develop innovative software solutions, earning the runner-up position for our strategic and creative approach to the given challenges."
+    },
+    {
+      jobtitle: "A2SV Hackathon Semi-Finalist",
+      where: "A2SV Coding Hackathon",
+      date: "2023",
+      description: "Competed against top coders and reached the semi-finals by demonstrating strong technical skills and the ability to work under tight deadlines."
+    },
+  ];
+  
+const skills = [{
+        name: "Python",
+        value: 50,
+    },
+    {
+        name: "Django",
+        value: 30,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 50,
+    },
+    {
+        name: "HTML, CSS AND PHP",
+        value: 85,
+    },
+];
+
+const services = [
+    {
+      title: "Internship at Skyworld Limited",
+      description: "Gaining practical experience in cutting-edge web development technologies and contributing to major projects as part of the dynamic Skyworld Limited team.",
+    },
+    {
+      title: "Oracle Hackathon Runner-Up",
+      description: "Demonstrated exceptional problem-solving skills and innovative thinking to secure the runner-up position at the Oracle onboarded hackathon.",
+    },
+    {
+      title: "A2SV Hackathon Semi-Finalist",
+      description: "Reached the semi-finals in the A2SV hackathon, showcasing proficiency in collaborative software development and rapid prototyping under pressure.",
+    },
+  ];
+  
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "newtonbryan12428@gmail.com",
+    YOUR_FONE: "(+254)0110952788",
+    description: "Energetic and passionate Web Development Intern with a solid foundation in both front-end and back-end programming. Adept at translating complex solutions into actionable development plans, I am committed to leveraging my skills in HTML, CSS, JavaScript, and responsive design to create engaging user experiences. My enthusiasm for technology extends to mobile development, where I explore the intricacies of creating cross-platform applications that seamlessly integrate with web services",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/newtvn",
+    linkedin: "https://www.linkedin.com/in/newton-nyongesa-356253226/",
+    Instagram: "https://www.instagram.com/newtvn/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
