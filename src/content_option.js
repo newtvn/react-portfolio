416d176ceb6298791671cf2e9b69c5c71dedb161
Newtvn <newtonbryan12428@gmail.com>
2025-02-26 11:10:21 +0300

    update 2

--- src/content_option.js
+++ src/content_option.js
@@ -1,18 +1,18 @@
 const logotext = "Newton Brian";
 const meta = {
     title: "Newton Brian Nyongesa",
-    description: "Newton Brian Nyongesa _ Full stack devloper,currently working interning at Sky World Limited",
+    description: "Newton Brian Nyongesa _ Full stack devloper,currently working an internship at Bitz.itc company ",
 };
 
 const introdata = {
     title: "Newton Brian Nyongesa",
     animated: {
-        first: "Founder and Director of Nexwave Corporations",
+        first: "UI/UX Designer and Developer",
         second: "Tech-fanatic",
         third: "Full stack developer",
     },
     description: "Computer science majour, French minor with a key interest in all systems tech and inovation",
-    your_img_url: "https://images.unsplash.com/photo-1605379399642-870262d3d051?q=80&w=1812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    your_img_url: "https://unsplash.com/photos/macbook-pro-on-black-wooden-table-PNbDkQ2DDgM",
 };
 
 const dataabout = {
@@ -23,13 +23,19 @@ const worktimeline =[
     {
       jobtitle: "Full stack Development Intern",
       where: "Skyworld Limited",
-      date: "Present",
+      date: "2024",
       description: "Engaged in full-stack web development projects, utilizing modern frameworks and languages to create responsive and user-centric solutions."
+    },
+    {
+      jobtitle: "UI/UX designer ",
+      where: "Wordandlearn",
+      date: "Present",
+      description: "Worked on the design and UI aspects of the Wordandlearn application, focusing on creating an intuitive and engaging user experience.",
     },
     {
       jobtitle: "Analyst and Cross functional sentiment evaluation intern",
       where: "Beats by Dre",
-      date: "Present",
+      date: "2024",
       description: "Gaining practical experience in cutting-edge sound engineering and proforma generation with ventures into reproduction and wavelength manipulation",
     },
     {
@@ -173,6 +179,21 @@ const dataportfolio = [
       description: "Secure me-A web based platform aimed at easening the process of analysis and relationship detection in the current criminal system.",
         link: "https://github.com/newtvn/secureme"
     },
+      {
+          img: "https://files.oaiusercontent.com/file-CkiLYTNrEzQ3oVmE1Tl5YO1U?se=2024-04-01T14%3A58%3A21Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dc50ca47e-a7da-44a8-816b-fa726cca686d.webp&sig=VN89QftPWJ/vnBwGTNXzKFJ58WvruN7Ub92tL4hmGbw%3D",
+          description: "The Sky Swift Figma design represents a modern logistics management platform, optimizing freight and shipment tracking for efficiency and real-time monitoring.",
+          link: "https://www.figma.com/design/hc3d5ZDSFoqLZDdxKsYhks/SkyLogistics?node-id=0-1&t=lwqQ9NBekx9KshNM-1"
+      },
+      {
+          img: "https://files.oaiusercontent.com/file-CkiLYTNrEzQ3oVmE1Tl5YO1U?se=2024-04-01T14%3A58%3A21Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dc50ca47e-a7da-44a8-816b-fa726cca686d.webp&sig=VN89QftPWJ/vnBwGTNXzKFJ58WvruN7Ub92tL4hmGbw%3D",
+          description: "The Arifu Management Dashboard Figma design showcases an intuitive platform for digital learning and engagement, empowering users with seamless content management.",
+          link: "https://www.figma.com/design/UDD5MjFlEyD5Iy3snRU468/Arifu-Management-Dashboard?node-id=0-1&t=gdVoEMRkvxrzrWTO-1"
+      },
+      {
+          img: "https://files.oaiusercontent.com/file-CkiLYTNrEzQ3oVmE1Tl5YO1U?se=2024-04-01T14%3A58%3A21Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dc50ca47e-a7da-44a8-816b-fa726cca686d.webp&sig=VN89QftPWJ/vnBwGTNXzKFJ58WvruN7Ub92tL4hmGbw%3D",
+          description: "The Zuri Health HRM Figma design outlines a robust human resource management system tailored for healthcare, ensuring efficient workforce tracking and operations.",
+          link: "https://www.figma.com/design/1qnn9BQeS87HuNReufQ60r/Zuri-Hackathon-HRM?node-id=72-982&t=T1nNEF9Jn7KfAtih-0"
+      },  
     {
       img: "https://files.oaiusercontent.com/file-CkiLYTNrEzQ3oVmE1Tl5YO1U?se=2024-04-01T14%3A58%3A21Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dc50ca47e-a7da-44a8-816b-fa726cca686d.webp&sig=VN89QftPWJ/vnBwGTNXzKFJ58WvruN7Ub92tL4hmGbw%3D",
       description: "The File Verification Application is designed to validate and process CSV files containing user data. It ensures that each entry in the CSV file has a valid ID number, mobile number, and email address as per the specified formats.",
